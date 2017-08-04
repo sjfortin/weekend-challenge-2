5f962d6b46259bcc8c8fd15372ab50d7e1bc1762
Sam Fortin <sam.j.fortin@gmail.com>
2017-08-04 16:50:32 -0500

    Starting point for client.js. Saving progress.

--- public/client.js
+++ public/client.js
@@ -1,3 +1,13 @@
 $(document).ready(function () {
-    
+
+        var numberOneValue = $('#number1').val();
+        var numberTwoValue = $('#number2').val();
+        var operator;
+        $('#selectOperation').on('click', '.operator', function(){
+            if($(this)[0].id === 'add') {
+                operator = '+';
+                console.log(operator);
+                
+            }
+        });
 });
