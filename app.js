18016665de0b39c8d9e4aa00283a1ea4d569befc
Sam Fortin <sam.j.fortin@gmail.com>
2017-08-04 22:39:33 -0500

    Calculation working. Not all logic on the server yet

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
 
 var port = 5000;
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+var calculation;
+
+app.post('/calculation', function(req,res){
+    console.log(req.body);
+    calculation = req.body.numberOneValue + req.body.operation + req.body.numberTwoValue;
+    res.sendStatus(201); 
+});
+
+app.get('/theCalculation', function (req, res) {
+    res.send(calculation);
+});
 
 app.listen(port, function(){
     console.log('Listening on port', port); 
--- public/client.js
+++ public/client.js
@@ -1,13 +1,56 @@
 $(document).ready(function () {
 
-        var numberOneValue = $('#number1').val();
-        var numberTwoValue = $('#number2').val();
-        var operator;
-        $('#selectOperation').on('click', '.operator', function(){
-            if($(this)[0].id === 'add') {
-                operator = '+';
-                console.log(operator);
-                
+    // Set the operator selected
+    var operator;
+
+    $('#selectOperation').on('click', '.operator', function () {
+        if ($(this)[0].id === 'add') {
+            operator = '+';
+            $('.operator').removeClass('active-operator');
+            $(this).addClass('active-operator');
+        } else if ($(this)[0].id === 'subtract') {
+            operator = '-';
+            $('.operator').removeClass('active-operator');
+            $(this).addClass('active-operator');
+        } else if ($(this)[0].id === 'multiply') {
+            operator = '*';
+            $('.operator').removeClass('active-operator');
+            $(this).addClass('active-operator');
+        } else if ($(this)[0].id === 'divide') {
+            operator = '/';
+            $('.operator').removeClass('active-operator');
+            $(this).addClass('active-operator');
+        }
+    });
+
+    // POST the calculation elements to the server
+    $('#submitCalculation').on('click', function(){
+        var numberOneValue = $('#numberOne').val();
+        var numberTwoValue = $('#numberTwo').val();
+        
+        $.ajax({
+            method: 'POST',
+            url: '/calculation',
+            data: {
+                numberOneValue: numberOneValue,
+                numberTwoValue: numberTwoValue,
+                operation: operator
+            },
+            success: function (response) {
+                console.log(response);
+                getCalculation();
             }
-        });
+        })
+    });
 });
+
+// GET the final calucation logic from the server
+function getCalculation() {
+    $.ajax({
+        method: 'GET',
+        url: '/theCalculation',
+        success: function (response) {
+            console.log(eval(response));
+        }
+    });
+}
