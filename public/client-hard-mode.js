cd95cfdf893cac1e27d3c4675376d570f55d09cb
Sam Fortin <sam.j.fortin@gmail.com>
2017-08-06 00:18:20 -0500

    Fixing bug that allows user to enter more than one decimal

--- public/client-hard-mode.js
+++ public/client-hard-mode.js
@@ -10,6 +10,8 @@ var firstNumber = '';
 var secondNumber = '';
 var operator;
 var operatorClicked = false;
+var firstNumberDecimalClicked = false;
+var secondNumberDecimalClicked = false;
 
 $(document).ready(function () {
     $('#calculator').on('click', '.calc-button', function () {
@@ -17,11 +19,21 @@ $(document).ready(function () {
         // If button clicked is a number or decimal
         if ($(this).attr('data-value')) {
             if (operatorClicked === false) {
-                firstNumber += $(this).data().value;
+                if (firstNumberDecimalClicked === false || (firstNumberDecimalClicked === true && $(this).data().value !== '.')) {
+                    firstNumber += $(this).data().value;
+                }
+                if ($(this).data().value === '.') {
+                    firstNumberDecimalClicked = true;
+                }
                 console.log('first number is ' + firstNumber);
                 $('#output').text(firstNumber);
             } else if (operatorClicked == true) {
-                secondNumber += $(this).data().value;
+                if (secondNumberDecimalClicked === false || (secondNumberDecimalClicked === true && $(this).data().value !== '.')) {
+                    secondNumber += $(this).data().value;
+                }
+                if ($(this).data().value === '.') {
+                    secondNumberDecimalClicked = true;
+                }
                 console.log('second number is ' + secondNumber);
                 $('#output').text(secondNumber);
             }
